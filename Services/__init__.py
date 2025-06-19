d0e5457b44bacfafbd9b8d94dd7b19bc1105d187
root <root@sulaco.localdomain>
2025-06-19 21:36:33 +0200

    cleanup old folders

--- Services/__init__.py
+++
@@ -1,5 +0,0 @@
-from JedoxPy.Services import ConnectionService
-from JedoxPy.Services import DatabaseService
-from JedoxPy.Services import CubeService
-from JedoxPy.Services import CellService
-from JedoxPy.Services import ElementService
--- Utils/Utils.py
+++
@@ -1,5 +0,0 @@
-def format_id_string(ids: list) -> str:
-    return ",".join(
-        str(x) if not isinstance(x, list) else ":".join(map(str, x))
-        for x in ids
-    )
